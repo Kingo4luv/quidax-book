9d102de0e7c8a8bfd4fa1bd57e1d1742ed1ec63d
Kingsley Atanang <Kingo4luv@yahoo.com>
2021-03-22 10:07:57 +0100

    change to class

--- src/Components/Home/FeaturedBook/index.js
+++ src/Components/Home/FeaturedBook/index.js
@@ -2,27 +2,36 @@ import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
 import FeaturedBook from "./book";
+import { Component } from 'react';
 
-const options = {
-    loop: true,
-        margin: 10,
-        responsive: {
-            0: {
-                items: 2,
-            },
-            600: {
-                items: 4,
-            },
-            1000: {
-                items: 6,
-            },
-            1200: {
-                items: 8,
-            }
-        }
-};
 
-const Featured = ({books}) => {
+
+class Featured extends Component{
+
+   render(){
+       const self = this;
+       const options = {
+           loop: true,
+           dots: true,
+               margin: 10,
+               responsive: {
+                   0: {
+                       items: 2,
+                   },
+                   600: {
+                       items: 4,
+                   },
+                   1000: {
+                       items: 6,
+                   },
+                   1200: {
+                       items: 8,
+                   }
+           },
+           onInitialized: function () {
+               self.carousel = this;
+           }
+       }
     return(
         <div id="featured-books">
             <div className="title-section">
@@ -30,26 +39,27 @@ const Featured = ({books}) => {
             </div>
             <div className="featured-container">
                 <div className="featured-books-wrapper">
-                    <OwlCarousel className='owl-theme' {...options}  nav> 
+                    <OwlCarousel className='owl-theme' {...options}> 
                         {
-                            books.filter(book => book.featured !== null).map((book, i) => {
+                            this.props.books.filter(book => book.featured !== null).map((book, i) => {
                             return(
                                 <FeaturedBook book={book} key={i}/>
                             )
                         })}
                     </OwlCarousel>
-                    <div className="left-nav">
+                    <div className="left-nav"  onClick={() => this.carousel.prev()}>
                         <svg width="6" height="11" viewBox="0 0 6 11" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path fillRule="evenodd" clipRule="evenodd" d="M5.5 10.5L5.49846 0.5L0.5 5.5L5.5 10.5Z" fill="black"/>
                         </svg>
                     </div>
-                    <div className="right-nav">
+                    <div className="right-nav" onClick={() => this.carousel.next()}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 512 512"><title>ionicons-v5-b</title><polygon points="144 448 368 256 144 64 144 448"/></svg>
                     </div>
                 </div>
             </div>
         </div>
     )
+    }
 }
 
 export default Featured;
